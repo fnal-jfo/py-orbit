380bda71221dd640fa442bb03b527bdd3c482248
Jean-Francois Ostiguy <ostiguy@sbcglobal.net>
2023-01-18 11:50:56 -0600

    Env variable settings for private setup.

---
+++ setenv-private.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash 
+export ORBIT_ROOT=/usr/local/repos/pyORBIT/Public/py-orbit
+export PYTHON_ROOT_INC=/usr/local/pyorbit/include/python2.7
+export PYTHON_ROOT_LIB=/usr/local/pyorbit/lib/libpython2.7.a
+export PYTHONPATH=${ORBIT_ROOT}/py:${ORBIT_ROOT}/lib:${PYTHONPATH}
